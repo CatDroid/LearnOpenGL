f382e39cab6a8e7d9e63315355d7e0d5cf3975f4
hohanloong <1198432354@qq.com>
2022-06-26 08:47:08 +0800

    gl_FragCoord是屏幕坐标系 xyz分量都是从0~1; 屏幕空间和标准设备空间都是左手坐标系

--- src/4.advanced_opengl/1.2.depth_testing_view/1.2.depth_testing.fs
+++ src/4.advanced_opengl/1.2.depth_testing_view/1.2.depth_testing.fs
@@ -6,11 +6,16 @@ float far = 100.0;
 float LinearizeDepth(float depth) 
 {
     float z = depth * 2.0 - 1.0; // back to NDC 
-    return (2.0 * near * far) / (far + near - z * (far - near));	
+    return (2.0 * near * far) / (far + near - z * (far - near));
+    // 透视投影的逆  转换为相机空间的Near到Far  Near/Far接近黑色  Far/Far=1远的是白色
+    // 注意，这里有做取反了  Zn = (f+n)/(f-n) + 2fn/(f-n) * 1/Ze    Ze = [2fn/(f-n) ]/ [Zn - (f+n)/(f-n)] = 2fn / [Zn(f-n)- (f+n)] 因为Ze是从-Near到-Far 这里取反
 }
 
 void main()
 {             
-    float depth = LinearizeDepth(gl_FragCoord.z) / far; // divide by far to get depth in range [0,1] for visualization purposes
+    float depth = LinearizeDepth(gl_FragCoord.z) / far; // divide by far to get depth in range [0,1] for visualization purposes 为了可视效果 除以far
     FragColor = vec4(vec3(depth), 1.0);
+    
+    
+    //FragColor = vec4(gl_FragCoord.zzz, 1.0);//  屏幕空间的非线性的深度值，物体稍微远一点就变成很白色了，
 }
