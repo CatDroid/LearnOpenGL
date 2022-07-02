1d18f3b2964d86c248bfa1c1ba14bcdc2f640831
CatDroid <460489017@qq.com>
2022-07-02 11:26:37 +0800

    ubo一致块对象的使用场合: 大量program ; 一次修改; uniform数量限制

--- src/4.advanced_opengl/8.advanced_glsl_ubo/8.advanced_glsl.vs
+++ src/4.advanced_opengl/8.advanced_glsl_ubo/8.advanced_glsl.vs
@@ -6,6 +6,15 @@ layout (std140) uniform Matrices
     mat4 projection;
     mat4 view;
 };
+
+// 基准偏移量 和 对齐偏移量(相对于块开头的偏移,并且一定是基准偏移量整数倍) 
+
+// std140 按照固定规则 
+// 注意 "数组" 不管是标量或向量, 每个元素的   基准对齐量, 都是4N(vec4,16个字节) , 比较耗内存
+//        "矩阵"                           每个列向量的基准对齐量, 都是4N(vec4,16个字节)
+// shared 不同的program的uniform都是一样的排序, 但是按照硬件最优化方式
+// packed 不同的program的uniform排序不一样, shared/packed都要用glGetUniformIndices查询
+
 uniform mat4 model;
 
 void main()
--- src/4.advanced_opengl/8.advanced_glsl_ubo/advanced_glsl_ubo.cpp
+++ src/4.advanced_opengl/8.advanced_glsl_ubo/advanced_glsl_ubo.cpp
@@ -135,27 +135,60 @@ int main()
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 
+	/*
+		UBO对象作用:
+			1. 任何大型的渲染程序都可能同时激活有上百个着色器程序，
+			    这时候Uniform缓冲对象的优势就会很大地体现出来了
+
+			2. 比起在多个着色器中修改同样的uniform，在Uniform缓冲中修改一次会更容易一些
+
+			3. 可以在着色器中使用更多的uniform。
+			    OpenGL限制了它能够处理的uniform数量，
+				这可以通过GL_MAX_VERTEX_UNIFORM_COMPONENTS来查询
+	*/
+
     // configure a uniform buffer object
     // ---------------------------------
-    // first. We get the relevant block indices
+    // first. We get the relevant block indices 
+	// 每个program中的"Matrices"一致块的句柄
+	// Uniform块索引(Uniform Block Index)是着色器中已定义Uniform块的位置值索引。
+	// 这可以通过调用glGetUniformBlockIndex来获取
     unsigned int uniformBlockIndexRed = glGetUniformBlockIndex(shaderRed.ID, "Matrices");
     unsigned int uniformBlockIndexGreen = glGetUniformBlockIndex(shaderGreen.ID, "Matrices");
     unsigned int uniformBlockIndexBlue = glGetUniformBlockIndex(shaderBlue.ID, "Matrices");
     unsigned int uniformBlockIndexYellow = glGetUniformBlockIndex(shaderYellow.ID, "Matrices");
+
+	// 在OpenGL上下文中，定义了一些绑定点(Binding Point)
+	// 在使用上, 一个绑定点 对应一个 UBO(链接到一个UBO), 相当于一个绑定点代表某种"功能"(比如绑定点0对应的是VP矩阵)
+	// 然后不同的program的对应uniform句柄(值不一定一样), 然后绑定到绑定点
+#define GL_CONTEXT_BIND_POINT_0 0 
+
+	// OpengGL4.2 shader中可以直接指定某个一致块的绑定点 不用调用 glGetUniformBlockIndex 和 glUniformBlockBinding
+	// 添加一个布局标识符  binding = 2
+	// layout(std140, binding = 2) uniform Lights { ... };
+
+	// 每个program中的一致块句柄 绑定到其中一个绑定点
+	// glUniformBlockBinding(着色器的句柄, 着色器中该一致块的句柄, 绑定点)
     // then we link each shader's uniform block to this uniform binding point
-    glUniformBlockBinding(shaderRed.ID, uniformBlockIndexRed, 0);
-    glUniformBlockBinding(shaderGreen.ID, uniformBlockIndexGreen, 0);
-    glUniformBlockBinding(shaderBlue.ID, uniformBlockIndexBlue, 0);
-    glUniformBlockBinding(shaderYellow.ID, uniformBlockIndexYellow, 0);
-    // Now actually create the buffer
+    glUniformBlockBinding(shaderRed.ID,		uniformBlockIndexRed,			GL_CONTEXT_BIND_POINT_0);
+    glUniformBlockBinding(shaderGreen.ID,	uniformBlockIndexGreen,		GL_CONTEXT_BIND_POINT_0);
+    glUniformBlockBinding(shaderBlue.ID,		uniformBlockIndexBlue,			GL_CONTEXT_BIND_POINT_0);
+    glUniformBlockBinding(shaderYellow.ID,	uniformBlockIndexYellow,		GL_CONTEXT_BIND_POINT_0);
+
+    // Now actually create the buffer 创建UBO, 大小刚好是两个Mat4的size
     unsigned int uboMatrices;
     glGenBuffers(1, &uboMatrices);
     glBindBuffer(GL_UNIFORM_BUFFER, uboMatrices);
     glBufferData(GL_UNIFORM_BUFFER, 2 * sizeof(glm::mat4), NULL, GL_STATIC_DRAW);
     glBindBuffer(GL_UNIFORM_BUFFER, 0);
-    // define the range of the buffer that links to a uniform binding point
-    glBindBufferRange(GL_UNIFORM_BUFFER, 0, uboMatrices, 0, 2 * sizeof(glm::mat4));
 
+
+	// 把UBO对象链接到一个绑定点 
+    // define the range of the buffer that links to a uniform binding point --可以一个UBO的不同部分对应不同的绑定点
+    glBindBufferRange(GL_UNIFORM_BUFFER, GL_CONTEXT_BIND_POINT_0, uboMatrices,  0,  2 * sizeof(glm::mat4));
+	// glBindBufferBase(GL_UNIFORM_BUFFER, GL_CONTEXT_BIND_POINT_0, uboExampleBlock); 
+
+	// 投影矩阵只更新一次 (导致了 投影矩阵的视野(Field of View)值保持不变, 所以摄像机就没有缩放了)
     // store the projection matrix (we only do this once now) (note: we're not using zoom anymore by changing the FoV)
     glm::mat4 projection = glm::perspective(45.0f, (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
     glBindBuffer(GL_UNIFORM_BUFFER, uboMatrices);
@@ -181,6 +214,7 @@ int main()
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+		// 每次只更新ubo对象中的view矩阵 (偏移+大小)
         // set the view and projection matrix in the uniform block - we only have to do this once per loop iteration.
         glm::mat4 view = camera.GetViewMatrix();
         glBindBuffer(GL_UNIFORM_BUFFER, uboMatrices);
@@ -193,7 +227,7 @@ int main()
         shaderRed.use();
         glm::mat4 model = glm::mat4(1.0f);
         model = glm::translate(model, glm::vec3(-0.75f, 0.75f, 0.0f)); // move top-left
-        shaderRed.setMat4("model", model);
+        shaderRed.setMat4("model", model); // 不同物体的模型矩阵还是要单独传入
         glDrawArrays(GL_TRIANGLES, 0, 36);
         // GREEN
         shaderGreen.use();
