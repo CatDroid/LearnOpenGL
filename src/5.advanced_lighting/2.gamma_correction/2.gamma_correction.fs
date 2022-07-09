a915815a8e0cf231ee00ef7e8eb6f67f5024f9c2
CatDroid <460489017@qq.com>
2022-07-09 14:07:58 +0800

    点光源的光线衰减: 与距离成反比,
    1.  公式有常数项Kc=1  一次项Kl 二次项Kq;
    2. Kq比较小, 二次项在距离近影响少, 距离大影响大, 使衰减速度变快, 但最后都会贴近0缓慢

--- src/5.advanced_lighting/2.gamma_correction/2.gamma_correction.fs
+++ src/5.advanced_lighting/2.gamma_correction/2.gamma_correction.fs
@@ -32,6 +32,28 @@ vec3 BlinnPhong(vec3 normal, vec3 fragPos, vec3 lightPos, vec3 lightColor)
     float distance = length(lightPos - fragPos);
     float attenuation = 1.0 / (gamma ? distance * distance : distance);
     
+	/*
+	  总结:
+	  1. 
+
+	  2. 点光源的光照衰减---常数项Kc、一次项Kl和二次项Kq
+	      float attenuation = 1.0 / (light.constant + light.linear * distance + 
+                light.quadratic * (distance * distance));
+		
+		  常数项通常保持为1.0，它的主要作用是保证分母永远不会比1小，否则的话在某些距离上它反而会增加强度，这肯定不是我们想要的效果。
+		  一次项会与距离值相乘，以线性的方式减少强度
+		  二次项会与距离的平方相乘，让光源以二次递减的方式减少强度。
+
+		  二次项在距离比较小的时候影响会比一次项小很多，
+		  但当距离值比较大的时候它就会比一次项更大了。
+
+		  由于二次项的存在，光线会在大部分时候以线性的方式衰退，直到距离变得足够大，
+		  让二次项超过一次项，光的强度会以更快的速度下降。
+		  (自己理解:二次项其作用的时候,会使下降速度比一次项要大,但是两者最后都会贴近0)
+
+		  一次项Kl为了覆盖更远的距离通常都很小，二次项Kq甚至更小
+	*/
+
     diffuse *= attenuation;
     specular *= attenuation;
     
