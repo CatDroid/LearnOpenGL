4e220fe44c9ec7f2f64b88664319e279263566f2
CatDroid <460489017@qq.com>
2022-07-09 22:01:31 +0800

    阴影: 渲染真正场景时, 从光源矩阵(VP)得到齐次坐标后做透视除法, 再*0.5+0.5后 就可以用xy来采样, z来做对比(xyz都是0~1) , 深度纹理存储的也是0~1

--- src/5.advanced_lighting/3.1.2.shadow_mapping_base/3.1.2.shadow_mapping.fs
+++ src/5.advanced_lighting/3.1.2.shadow_mapping_base/3.1.2.shadow_mapping.fs
@@ -16,15 +16,19 @@ uniform vec3 viewPos;
 
 float ShadowCalculation(vec4 fragPosLightSpace)
 {
-    // perform perspective divide
+    // perform perspective divide ps阶段才做透视除法
     vec3 projCoords = fragPosLightSpace.xyz / fragPosLightSpace.w;
-    // transform to [0,1] range
+    
+	// transform to [0,1] range  注意这里把深度从1~-1 也转换到了 1~0 
     projCoords = projCoords * 0.5 + 0.5;
-    // get closest depth value from light's perspective (using [0,1] range fragPosLight as coords)
-    float closestDepth = texture(shadowMap, projCoords.xy).r; 
-    // get depth of current fragment from light's perspective
+    
+	// get closest depth value from light's perspective (using [0,1] range fragPosLight as coords)
+    float closestDepth = texture(shadowMap, projCoords.xy).r;  // 只要r分量
+    
+	// get depth of current fragment from light's perspective
     float currentDepth = projCoords.z;
-    // check whether current frag pos is in shadow
+    
+	// check whether current frag pos is in shadow 1.0代表是阴影
     float shadow = currentDepth > closestDepth  ? 1.0 : 0.0;
 
     return shadow;
@@ -35,20 +39,27 @@ void main()
     vec3 color = texture(diffuseTexture, fs_in.TexCoords).rgb;
     vec3 normal = normalize(fs_in.Normal);
     vec3 lightColor = vec3(0.3);
+
     // ambient
     vec3 ambient = 0.3 * lightColor;
+
     // diffuse
     vec3 lightDir = normalize(lightPos - fs_in.FragPos);
     float diff = max(dot(lightDir, normal), 0.0);
     vec3 diffuse = diff * lightColor;
+
+
     // specular
     vec3 viewDir = normalize(viewPos - fs_in.FragPos);
-    vec3 reflectDir = reflect(-lightDir, normal);
+    //vec3 reflectDir = reflect(-lightDir, normal);
     float spec = 0.0;
+	float shininess =  64.0;
     vec3 halfwayDir = normalize(lightDir + viewDir);  
-    spec = pow(max(dot(normal, halfwayDir), 0.0), 64.0);
-    vec3 specular = spec * lightColor;    
-    // calculate shadow
+    spec = pow(max(dot(normal, halfwayDir), 0.0), shininess);
+    vec3 specular = spec * lightColor;  
+	
+    // calculate shadow 阴影的话没有diffuse和specular
+	//  (diffuse可能不是0, 表面法线和光源方向可能一致,但被挡住了)
     float shadow = ShadowCalculation(fs_in.FragPosLightSpace);                      
     vec3 lighting = (ambient + (1.0 - shadow) * (diffuse + specular)) * color;    
     
--- src/5.advanced_lighting/3.1.2.shadow_mapping_base/3.1.2.shadow_mapping.vs
+++ src/5.advanced_lighting/3.1.2.shadow_mapping_base/3.1.2.shadow_mapping.vs
@@ -6,10 +6,10 @@ layout (location = 2) in vec2 aTexCoords;
 out vec2 TexCoords;
 
 out VS_OUT {
-    vec3 FragPos;
-    vec3 Normal;
+    vec3 FragPos; //世界空间  ps计算 blinn-phong的视线和光线方向 
+    vec3 Normal;  //世界空间  ps计算 Blinn-phong 
     vec2 TexCoords;
-    vec4 FragPosLightSpace;
+    vec4 FragPosLightSpace; //光源的齐次空间 采集阴影图 
 } vs_out;
 
 uniform mat4 projection;
@@ -22,6 +22,7 @@ void main()
     vs_out.FragPos = vec3(model * vec4(aPos, 1.0));
     vs_out.Normal = transpose(inverse(mat3(model))) * aNormal;
     vs_out.TexCoords = aTexCoords;
+	// 注意这个没有做透视除法
     vs_out.FragPosLightSpace = lightSpaceMatrix * vec4(vs_out.FragPos, 1.0);
     gl_Position = projection * view * model * vec4(aPos, 1.0);
 }
