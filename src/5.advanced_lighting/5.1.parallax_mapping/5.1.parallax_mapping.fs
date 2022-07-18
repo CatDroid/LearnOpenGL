37494c01e398d44e9fdef540b955c29b73175ad7
hohanloong <1198432354@qq.com>
2022-07-19 00:21:28 +0800

    视差贴图: viewDir.xy/viewDir.z 是为了在不同角度看得到不同的uv坐标偏移

--- src/5.advanced_lighting/5.1.parallax_mapping/5.1.parallax_mapping.fs
+++ src/5.advanced_lighting/5.1.parallax_mapping/5.1.parallax_mapping.fs
@@ -17,6 +17,18 @@ uniform float heightScale;
 
 vec2 ParallaxMapping(vec2 texCoords, vec3 viewDir)
 { 
+ 
+	// 1. 即使是表面的同一点（采样的都是高度都是H(A)）,但是因为视角的不一样，
+	//     viewDir.xy/viewDIr.z * height 得到的偏移就不一样
+	//
+	// 2. viewDir是在切线空间
+	//     viewDir.z会在0.0到1.0之间的某处。
+	//     当viewDir大致平行于表面时，它的z元素接近于0.0，除法会返回比viewDir垂直于表面的时候更大的P向量。
+	//     所以，从本质上，相比正朝向表面，当带有角度地看向平面时，我们会更大程度地缩放P的大小，从而增加纹理坐标的偏移；
+	//     这样做在视角上会获得更大的真实度。
+
+	// 不除以viewDir.z --- 有偏移量限制的视差贴图（Parallax Mapping with Offset Limiting）
+	// 
     float height =  texture(depthMap, texCoords).r;     
     return texCoords - viewDir.xy * (height * heightScale);        
 }
