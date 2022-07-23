4a4a19577783d7d7f70f7a5dcc7a0b652f4757e6
hohanloong <1198432354@qq.com>
2022-07-24 01:12:55 +0800

    陡峭视差贴图: 分层多次采样,找到与"位移几何表面"相交后的首个P向量(深度图值比P向量深度层小); 视差遮蔽贴图: 找到相交前后的P向量对纹理坐标插值
    视差遮蔽贴图: 只是近似，因为计算过程把相交前后的深度图和深度层差，与位移几何表面交点，作为相似三角形，或者说中间端的位移几何表面看成直线

--- src/5.advanced_lighting/5.2.steep_parallax_mapping/5.2.parallax_mapping.fs
+++ src/5.advanced_lighting/5.2.steep_parallax_mapping/5.2.parallax_mapping.fs
@@ -16,33 +16,49 @@ uniform sampler2D depthMap;
 uniform float heightScale;
 
 vec2 ParallaxMapping(vec2 texCoords, vec3 viewDir)
-{ 
+{
+    // 根据我们看向表面的角度调整样本数量
     // number of depth layers
     const float minLayers = 8;
     const float maxLayers = 32;
-    float numLayers = mix(maxLayers, minLayers, abs(dot(vec3(0.0, 0.0, 1.0), viewDir)));  
+    float numLayers = mix(maxLayers, minLayers, abs(dot(vec3(0.0, 0.0, 1.0), viewDir)));
+    
+    
+    // 每一层的深度 z轴 0到1 分成numLayers层 深度
     // calculate the size of each layer
     float layerDepth = 1.0 / numLayers;
-    // depth of current layer
-    float currentLayerDepth = 0.0;
+    
+    
+    // viewDir.xy / viewDir.z 相当于深度是整个高度场/深度场 也就是视线从场的一边到达另外一边的xy offset作为变化总量
+    // viewDir.xy * heightScale; // 这个只是把viewDir单位向量的xy 乘以 scale 作为总变化量，有偏移量限制的视差贴图（Parallax Mapping with Offset Limiting）
     // the amount to shift the texture coordinates per layer (from vector P)
-    vec2 P = viewDir.xy / viewDir.z * heightScale; 
+    vec2 P = viewDir.xy / viewDir.z * heightScale;
+    
+    // 每次纹理坐标增加量
     vec2 deltaTexCoords = P / numLayers;
   
     // get initial values
+    // depth of current layer
+    float currentLayerDepth = 0.0;
     vec2  currentTexCoords     = texCoords;
     float currentDepthMapValue = texture(depthMap, currentTexCoords).r;
       
+      
+    // 循环 每一层 获取 深度图的值 和 当前P向量深度 对比, 如果当前P向量的深度(currentLayerDepth)要小, 说明还在物体表面上方,继续
     while(currentLayerDepth < currentDepthMapValue)
     {
         // shift texture coordinates along direction of P
+        // 由于ViewDir的方向跟P方向是相反
         currentTexCoords -= deltaTexCoords;
+        
+        // 获取新一层的深度图的深度 和 计算这层P向量的深度
         // get depthmap value at current texture coordinates
         currentDepthMapValue = texture(depthMap, currentTexCoords).r;  
         // get depth of next layer
         currentLayerDepth += layerDepth;  
     }
     
+    // 返回已经偏移的纹理坐标
     return currentTexCoords;
 }
 
@@ -52,6 +68,7 @@ void main()
     vec3 viewDir = normalize(fs_in.TangentViewPos - fs_in.TangentFragPos);
     vec2 texCoords = fs_in.TexCoords;
     
+    // 视差贴图--修改纹理坐标
     texCoords = ParallaxMapping(fs_in.TexCoords,  viewDir);       
     if(texCoords.x > 1.0 || texCoords.y > 1.0 || texCoords.x < 0.0 || texCoords.y < 0.0)
         discard;
--- src/5.advanced_lighting/5.3.parallax_occlusion_mapping/5.3.parallax_mapping.fs
+++ src/5.advanced_lighting/5.3.parallax_occlusion_mapping/5.3.parallax_mapping.fs
@@ -15,45 +15,85 @@ uniform sampler2D depthMap;
 
 uniform float heightScale;
 
+// Parallax Mapping with Offset Limiting 有偏移量限制的视差贴图
+// Steep Parallax Mapping 陡峭视差映射  1.分层 多次采样 2.视角调整样本数量  -- 视角水平看陡坡 锯齿效果以及图层之间有明显的断层，提高样本的数目
+// Parallax Occlusion Mapping 视差遮蔽映射 更经常使用 --  不是用触碰的第一个深度层的纹理坐标，而是在触碰之前和之后，在深度层之间进行线性插值
+// Relief Parallax Mapping 浮雕视差贴图  比 视差遮蔽映射 更精确一些 但 性能开销更多
+
+/*
+ 
+ 视差映射是增强场景细节的一种很好的技术，但在使用它时确实带有一些您必须考虑的伪影(artifacts)。
+ 大多数情况下，视差映射用于"地板或类似墙壁的表面"，
+ 
+ 在这些表面上不太容易确定determine表面的轮廓，并且视角通常大致垂直于表面。
+ 这样一来，视差映射的伪影(artifacts)就不会那么明显，
+ 并使其成为增强对象细节的一种非常有趣的技术。
+ 
+ 
+ Parallax Occlusion Mapping 提供了令人惊讶的好结果，
+ 尽管一些轻微的伪影(artifacts)和锯齿(aliasing)问题仍然可见，
+ 但它通常是一个很好的折衷方案，
+ 只有在大幅放大或查看非常陡峭的角度时才真正可见。
+ 
+ */
+
 vec2 ParallaxMapping(vec2 texCoords, vec3 viewDir)
-{ 
+{
     // number of depth layers
     const float minLayers = 8;
     const float maxLayers = 32;
-    float numLayers = mix(maxLayers, minLayers, abs(dot(vec3(0.0, 0.0, 1.0), viewDir)));  
+    float numLayers = mix(maxLayers, minLayers, abs(dot(vec3(0.0, 0.0, 1.0), viewDir))); // viewDir是切线空间的
+    
+    
     // calculate the size of each layer
     float layerDepth = 1.0 / numLayers;
-    // depth of current layer
-    float currentLayerDepth = 0.0;
+
+    
     // the amount to shift the texture coordinates per layer (from vector P)
-    vec2 P = viewDir.xy / viewDir.z * heightScale; 
-    vec2 deltaTexCoords = P / numLayers;
+    vec2 P = viewDir.xy / viewDir.z * heightScale;
+    vec2 deltaTexCoords = P / numLayers; 
+  
   
     // get initial values
+    // depth of current layer
+    float currentLayerDepth = 0.0;
     vec2  currentTexCoords     = texCoords;
     float currentDepthMapValue = texture(depthMap, currentTexCoords).r;
       
+    
     while(currentLayerDepth < currentDepthMapValue)
     {
         // shift texture coordinates along direction of P
         currentTexCoords -= deltaTexCoords;
+        
+        
         // get depthmap value at current texture coordinates
         currentDepthMapValue = texture(depthMap, currentTexCoords).r;  
         // get depth of next layer
         currentLayerDepth += layerDepth;  
     }
     
+    // 在找到 与(位移)表面几何(displaced)surface geometry相交后的深度层 之后
+    // 获取 相交前深度层的纹理坐标
+    
+    
     // get texture coordinates before collision (reverse operations)
     vec2 prevTexCoords = currentTexCoords + deltaTexCoords;
-
+    float beforeDepthMapValue = texture(depthMap, prevTexCoords).r;
+    float beforeLayerDepth = (currentLayerDepth - layerDepth);
+    
     // get depth after and before collision for linear interpolation
-    float afterDepth  = currentDepthMapValue - currentLayerDepth;
-    float beforeDepth = texture(depthMap, prevTexCoords).r - currentLayerDepth + layerDepth;
+    float afterDepth  = currentDepthMapValue - currentLayerDepth;  // 相交后的采样深度 和 相交后的层深度 的差 负数
+    //float beforeDepth = beforeDepthMapValue - currentLayerDepth + layerDepth;
+    float beforeDepth = beforeDepthMapValue - beforeLayerDepth;   // 相交后的采样深度 和 相交后的层深度 的差 正数
  
     // interpolation of texture coordinates
-    float weight = afterDepth / (afterDepth - beforeDepth);
+    float weight = afterDepth / (afterDepth - beforeDepth); // -afterDepth/(-afterDepth+beforeDepth) 近似看成相似三角形 
+    
+    // 线性插值是两层纹理坐标之间的基本插值
     vec2 finalTexCoords = prevTexCoords * weight + currentTexCoords * (1.0 - weight);
 
+    // 返回最终的插值纹理坐标
     return finalTexCoords;
 }
 
