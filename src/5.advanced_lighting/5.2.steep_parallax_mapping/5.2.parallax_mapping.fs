dc261d24224209739fdca4e5d598e0bdc289e0c0
hohanloong <1198432354@qq.com>
2022-07-24 15:51:31 +0800

    MIPMAP: GPU在纹理采样为确认level求梯度, 需要确认临近片元采样的纹理坐标, 这要求每个片段执行一样的代码; while和for循环会被展开,最后按逻辑取其中的一个结果
    普通视差贴图: 只在高度变化不大,  偏移不大的情况下,  失真较少

--- src/5.advanced_lighting/5.2.steep_parallax_mapping/5.2.parallax_mapping.fs
+++ src/5.advanced_lighting/5.2.steep_parallax_mapping/5.2.parallax_mapping.fs
@@ -15,6 +15,24 @@ uniform sampler2D depthMap;
 
 uniform float heightScale;
 
+/*
+
+ RayMarching 光线行进
+ RayTracing 光线追踪
+
+ ??? 一般的视差贴图, 假设入口点的高度与交叉点的高度相同。但仅当入口点和交叉点实际上具有相同的高度时，这才是正确的 ???
+ 当偏移量不大并且高度场变化不大时，它仍然可以很好地工作。
+ 然而，当“偏移变得太大”或“高度变化太快”时，我们最终会得到一个疯狂的猜测，这很可能是错误的。
+ 这就是导致表面撕裂的伪影的原因。
+ 
+ 
+ 如果我们能弄清楚光线实际击中高度场(height field)的位置(B向量)，那么我们总能找到真正的可见表面点。
+ 这不能用单个纹理样本来完成。
+ 我们必须沿着视线小步移动(更新P向量)，每次都对高度场进行采样，直到到达表面。
+ 这种技术被称为光线行进 raymarching。
+ 
+*/
+
 vec2 ParallaxMapping(vec2 texCoords, vec3 viewDir)
 {
     // 根据我们看向表面的角度调整样本数量
@@ -43,6 +61,34 @@ vec2 ParallaxMapping(vec2 texCoords, vec3 viewDir)
     vec2  currentTexCoords     = texCoords;
     float currentDepthMapValue = texture(depthMap, currentTexCoords).r;
       
+    /*
+        unity中编译可能有警告?
+            警告告诉我们循环中使用了"梯度指令"gradient instructions。这是指我们循环中的纹理采样.
+            GPU 必须确定要使用哪个 mipmap 级别--- (MIPMAP的级别 需要根据临近的片元使用的纹理坐标 来确定)
+            !!为此它需要比较相邻片段的已使用 UV 坐标(UV coordinates of adjacent fragments)
+     
+            !!只有当所有片段都执行相同的代码时，它才能做到这一点。
+     
+            这对我们的循环来说是不可能的，因为它可以提前终止，这可能因片段而异。
+     
+            因此编译器将展开循环，这意味着它将一直执行所有九个步骤，无论我们的逻辑是否表明我们可以提前停止。
+            相反，它使用确定性逻辑在之后选择最终结果。
+     
+     
+            是的，但我们必须摆脱渐变指令 gradient instructions。
+            这可以通过我们自己确定 UV 导数并手动控制 mipmap 级别来实现。
+     
+            (使用导数是一个高级主题，我不会在本教程中介绍)
+            
+            即使这样，片段也会被并行处理。
+            基本上，一起计算的一批片段的性能, 是由需要最多迭代的片段决定的。
+            (the performance of a batch of fragments that are computed together is determined by the fragment that requires the most iterations.)
+            
+            因此，任何潜在的性能提升都是可变的和不可预测的，并且会因 GPU 而异。
+            需要进行广泛的测试以确定哪种方法最适合特定硬件。
+     
+     
+     */
       
     // 循环 每一层 获取 深度图的值 和 当前P向量深度 对比, 如果当前P向量的深度(currentLayerDepth)要小, 说明还在物体表面上方,继续
     while(currentLayerDepth < currentDepthMapValue)
