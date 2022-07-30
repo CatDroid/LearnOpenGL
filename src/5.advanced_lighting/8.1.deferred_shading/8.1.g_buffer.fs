1dca6ab14a1523bdaecb33e1740a8f51890dce50
hohanloong <1198432354@qq.com>
2022-07-30 09:32:57 +0800

    半浮点纹理: 内部format是GL_RGBA16F, format是GL_RGBA, 数据类型要是GL_HALF_FLOAT但也可以是GL_FLOAT

--- src/5.advanced_lighting/8.1.deferred_shading/8.1.g_buffer.fs
+++ src/5.advanced_lighting/8.1.deferred_shading/8.1.g_buffer.fs
@@ -3,6 +3,19 @@ layout (location = 0) out vec3 gPosition;  // MRT多路输出
 layout (location = 1) out vec3 gNormal;  // 注意 gPosition gNormal  都是浮点纹理 GL_RGBA16F + GL_RGBA + GL_FLOAT
 layout (location = 2) out vec4 gAlbedoSpec; //  gAlbedoSpec 是个定点纹理   GL_RGBA + GL_RGBA + GL_UNSIGN_BYTE
 
+/*
+Unsized Internal Format
+GL_RGB                         GL_RGB	    GL_UNSIGNED_BYTE, GL_UNSIGNED_SHORT_5_6_5	 
+GL_RGBA	                     GL_RGBA	    GL_UNSIGNED_BYTE, GL_UNSIGNED_SHORT_4_4_4_4, GL_UNSIGNED_SHORT_5_5_5_1
+
+Sized Internal Format        Format       Type
+GL_RGBA8	                       GL_RGBA	 GL_UNSIGNED_BYTE 
+GL_SRGB8_ALPHA8	       GL_RGBA    GL_UNSIGNED_BYTE
+
+GL_RGBA16F		              GL_RGBA     GL_HALF_FLOAT GL_FLOAT // 这里可以是FLOAT
+GL_RGBA32F		              GL_RGBA     GL_FLOAT
+GL_RGB565                     GL_RGB       GL_UNSIGNED_BYTE, GL_UNSIGNED_SHORT_5_6_5
+*/
 in vec2 TexCoords;
 in vec3 FragPos;
 in vec3 Normal;
